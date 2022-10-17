4c408de96c3e26dc4fbb5fa1ec28c3d9792145b1
Oluwatiseunla <abduloluwaseun16@gmail.com>
2022-10-17 13:56:42 -0700

    alpahabets in small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - alphabet in lower case
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch = 'z' ; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
