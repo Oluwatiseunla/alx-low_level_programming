732ef5060e700833afebfec70c398a3a6282c76f
Oluwatiseunla <abduloluwaseun16@gmail.com>
2022-10-17 14:07:55 -0700

    alpahabets in small letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.H>
 
 /**
  * main - Alaphabet in lowercase
@@ -11,10 +11,11 @@ int main(void)
 	char letter;
 
 	for (letter = 'a' ; letter <= 'z' ; letter++)
-	{
 		putchar(letter);
 
-	}
+	for (letter = 'A' ; letter <= 'Z' ; letter++)
+		putchar(letter);
+
 	putchar('\n');
 
 	return (0);
