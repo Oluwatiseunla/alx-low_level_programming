9383fec7780f9af5db965a9003d09b45d810537e
Oluwatiseunla <abduloluwaseun16@gmail.com>
2022-10-17 13:06:00 -0700

    print lower alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - Alaphabet in lowercase
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(ch);
+	return (0);
+}
