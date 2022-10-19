90154043815c5f325c2076b1ef82bd67f6f65cc4
Oluwatiseunla <abduloluwaseun16@gmail.com>
2022-10-18 19:27:00 -0700

    add _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-_putchar('i_');
-_putchar('p');
-_putchar('u');
-_putchar('r');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	_putchar('i_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('r');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 return (0);
 }
