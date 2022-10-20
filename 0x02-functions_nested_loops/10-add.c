b7b5c29d785b2df4c17bfb14ad490557c1cea158
Oluwatiseunla <abduloluwaseun16@gmail.com>
2022-10-19 20:43:20 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integres
+ * @a: first number
+ * @b: second number
+ *
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	_putchar(c);
+}
