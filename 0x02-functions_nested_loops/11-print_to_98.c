6daed7146d7e3893d69b675543a7c654ca9f56dc
Oluwatiseunla <abduloluwaseun16@gmail.com>
2022-10-19 21:14:53 -0700

    prints n to 998

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints number from n to 98
@@ -9,10 +9,17 @@
 
 void print_to_98(int n)
 {
-	for (n <= 98 ; n++ ;)
+	if (n >= 98)
 	{
-		_putchar(n);
-		_putchar(',');
-		_putchar(' ');
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n",  n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
