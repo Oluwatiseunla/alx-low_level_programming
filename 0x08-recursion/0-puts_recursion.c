9408e36e8da7b5cc113ed0458836dfb89458982e
Oluwatiseunla <abduloluwaseun16@gmail.com>
2022-11-02 14:55:05 -0700

    print a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,14 +2,12 @@
 
 /**
  * _puts_recursion - prints a string
- *@s: string
- *
+ * @s: string
  * Return: void
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s =='\0")
+	if  (s == '\0')
 	{
 		_putchar('\n');
 		return;
