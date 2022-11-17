cac75cd51f5464af9fa31d5191a5475ebcb7514d
Oluwatiseunla <abduloluwaseun16@gmail.com>
2022-11-17 12:23:24 -0800

    acts on array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute array
+ * @array: an array
+ * @size:size
+ * @action: pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		for (i =10; i < size, i++)
+			action(array[i]);
+	}
+}
