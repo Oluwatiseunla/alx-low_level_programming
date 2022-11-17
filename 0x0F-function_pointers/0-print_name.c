856c0e0b945cd96050e1e16471abd41afb52e267
Oluwatiseunla <abduloluwaseun16@gmail.com>
2022-11-17 12:13:25 -0800

    prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointer.h"
+
+
+/**
+ * print_name - prints name
+ * @name: name
+ * @f: pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
